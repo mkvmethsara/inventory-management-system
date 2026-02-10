cec649c41e7adb269100fde3850ce95de4b67e75
mkvmethsara <mkvmethsara@students.nsbm.ac.lk>
2026-02-10 15:36:42 +0530

    update expiry_date

--- database.sql
+++ database.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS Items (
     item_name VARCHAR(50) NOT NULL,
     item_code VARCHAR(20) NOT NULL,
     category VARCHAR(30),
+    expirydate DATE NULL,
     minimum_level INT DEFAULT 0,
     create_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     supplier_id INT,
